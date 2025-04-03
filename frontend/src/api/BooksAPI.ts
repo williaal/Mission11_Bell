e885ee82534e84415c1166182f6444547393c915
williaal <lailai26williams@gmail.com>
2025-04-03 17:56:50 -0600

    Finishing deployment

--- frontend/src/api/BooksAPI.ts
+++ frontend/src/api/BooksAPI.ts
@@ -5,7 +5,7 @@ interface FetchBooksResponse {
   totalNumBooks: number;
 }
 
-const API_URL = `https://localhost:5000/api/Book`;
+const API_URL = `https://mission13-bell-backend-d6guckemgcdeaveg.eastus-01.azurewebsites.net/api/Book`;
 export const fetchBooks = async (
   pageSize: number,
   pageNum: number,
