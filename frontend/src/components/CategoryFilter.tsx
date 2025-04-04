ffb0842fccc655f85f0c8b2f1ae858483ff8fc99
williaal <lailai26williams@gmail.com>
2025-04-03 18:02:56 -0600

    Fixed Category Link

--- frontend/src/components/CategoryFilter.tsx
+++ frontend/src/components/CategoryFilter.tsx
@@ -14,7 +14,7 @@ function CategoryFilter({
     const fetchCategories = async () => {
       try {
         const response = await fetch(
-          'https://localhost:5000/api/Book/GetBookTypes'
+          'https://mission13-bell-backend-d6guckemgcdeaveg.eastus-01.azurewebsites.net/api/Book/GetBookTypes'
         );
         const data = await response.json();
         console.log('Fetched categories: ', data);
